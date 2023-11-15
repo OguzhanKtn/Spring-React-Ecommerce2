931cd78752a1917a98b5748820c669d8967dd53d
Oğuzhan <81297977+OguzhanKtn@users.noreply.github.com>
2023-11-15 21:56:46 +0300

    Security

--- spring/src/main/java/com/works/configs/SecurityConfig.java
+++ spring/src/main/java/com/works/configs/SecurityConfig.java
@@ -1,14 +1,37 @@
 package com.works.configs;
 
+import com.works.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     final PasswordEncoder passwordEncoder;
+    final UserService userService;
+    final JwtFilter jwtFilter;
 
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder);
+    }
+
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+        .cors()
+        .and()
+        .csrf()
+        .disable()
+        .headers()
+        .frameOptions()
+        .deny()
+        .and()
+        .authorizeRequests()
+
+    }
 
 }
---
+++ spring/src/main/java/com/works/restcontrollers/ProductRestController.java
@@ -0,0 +1,48 @@
+package com.works.restcontrollers;
+
+import com.works.entities.Product;
+import com.works.services.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/product")
+@CrossOrigin
+public class ProductRestController {
+
+    final ProductService productService;
+
+    @PostMapping("/save")
+    public ResponseEntity productSave(@Valid @RequestBody Product product){
+        return productService.save(product);
+    }
+
+    @PostMapping("/delete/{pid}")
+    public ResponseEntity productDelete(@PathVariable Long pid){
+        return productService.productDelete(pid);
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity update(@RequestBody Product product){
+        return productService.updateProduct(product);
+    }
+
+    @GetMapping("/productList")
+    public ResponseEntity productList(@RequestParam(defaultValue = "0") int page){
+        return productService.allProducts(page);
+    }
+
+    @GetMapping("/listByCategory/{cid}")
+    public ResponseEntity listByCategory(@PathVariable int cid){
+        return productService.listByCategory(cid);
+    }
+
+    @GetMapping("/detail/{pid}")
+    public ResponseEntity detail(@PathVariable Long pid){
+        return productService.productDetail(pid);
+    }
+}
